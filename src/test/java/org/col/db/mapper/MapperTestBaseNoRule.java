a66df79c9a5695faff1e5392d0af393af66792b2
mdoering <mdoering@gbif.org>
2017-09-18 13:20:50 +0200

    Simplify test even more

--- src/test/java/org/col/db/mapper/MapperTestBaseNoRule.java
+++ src/test/java/org/col/db/mapper/MapperTestBaseNoRule.java
@@ -1,14 +1,11 @@
 package org.col.db.mapper;
 
-import jersey.repackaged.com.google.common.base.Throwables;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
 import java.net.ServerSocket;
-import java.time.Duration;
-import java.time.Instant;
 
 /**
  * A reusable base class for all mybatis mapper tests that takes care of postgres & mybatis.
@@ -32,33 +29,13 @@ public class MapperTestBaseNoRule<T> {
 
   @BeforeClass
   public static void before() throws Throwable {
-    startDb();
-  }
-
-  private static void startDb() {
-    System.out.println("Starting Postgres");
-    try {
-      postgres = new EmbeddedPostgres(Version.V9_6_3);
-      // assigned to some free port
-      ServerSocket socket = new ServerSocket(0);
-      final String database = "col";
-      final String user = "col";
-      final String password = "species2000";
-
-      Instant start = Instant.now();
-      System.out.println("Start postgres on port "+socket.getLocalPort());
-      final String url = postgres.start("localhost", socket.getLocalPort(), database, user, password);
-      System.out.format("Pg startup time: %s ms\n", Duration.between(start, Instant.now()).toMillis());
-
-    } catch (Exception e) {
-      System.err.println("Pg startup error: " + e.getMessage());
-      e.printStackTrace();
-
-      if (postgres != null) {
-        postgres.stop();
-      }
-      Throwables.propagate(e);
-    }
+    postgres = new EmbeddedPostgres(Version.V9_6_3);
+    // assigned to some free port
+    ServerSocket socket = new ServerSocket(0);
+    final String database = "col";
+    final String user = "col";
+    final String password = "species2000";
+    final String url = postgres.start("localhost", socket.getLocalPort(), database, user, password);
   }
 
   @AfterClass
