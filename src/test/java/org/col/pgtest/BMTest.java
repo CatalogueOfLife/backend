53ea5051baf6e71bff43036fb14329ee1ac6969f
mdoering <mdoering@gbif.org>
2017-09-18 12:37:13 +0200

    try with other credentials for embedded pg

--- src/test/java/org/col/pgtest/BMTest.java
+++ src/test/java/org/col/pgtest/BMTest.java
@@ -31,7 +31,7 @@ public class BMTest {
   @BeforeClass
   public static void initDB() throws IOException {
     embeddedPostgres = new EmbeddedPostgres(Version.V9_6_2);
-    jdbcUrl = embeddedPostgres.start("localhost", new ServerSocket(0).getLocalPort(), "testdb", "user", "password");
+    jdbcUrl = embeddedPostgres.start("localhost", new ServerSocket(0).getLocalPort(), "col", "col", "species2000");
     System.out.println("Postgres has started, yes!");
   }
 
