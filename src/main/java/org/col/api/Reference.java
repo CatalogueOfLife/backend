36bd8bf151d28c9063f5356176bb7007ee822565
mdoering <mdoering@gbif.org>
2017-09-26 11:38:31 +0200

    Add reference hash & equals methods

--- src/main/java/org/col/api/Reference.java
+++ src/main/java/org/col/api/Reference.java
@@ -1,5 +1,7 @@
 package org.col.api;
 
+import java.util.Objects;
+
 /**
  * Simplified literature reference class for proof of concept only.
  */
@@ -10,6 +12,21 @@ public class Reference {
   private Integer year;
   private String doi;
 
-  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Reference reference = (Reference) o;
+    return Objects.equals(key, reference.key) &&
+        Objects.equals(csl, reference.csl) &&
+        Objects.equals(serial, reference.serial) &&
+        Objects.equals(year, reference.year) &&
+        Objects.equals(doi, reference.doi);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(key, csl, serial, year, doi);
+  }
 }
 
