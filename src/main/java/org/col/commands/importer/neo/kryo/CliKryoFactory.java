c3e5b5b589bb05d13b59bd0197885f0384e53e88
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-09-26 11:32:26 +0200

    Added LocalDate

--- src/main/java/org/col/commands/importer/neo/kryo/CliKryoFactory.java
+++ src/main/java/org/col/commands/importer/neo/kryo/CliKryoFactory.java
@@ -1,17 +1,56 @@
 package org.col.commands.importer.neo.kryo;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.pool.KryoFactory;
-import it.unimi.dsi.fastutil.ints.IntArrayList;
-import org.col.api.*;
-import org.col.api.vocab.*;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
+
+import org.col.api.Dataset;
+import org.col.api.DatasetMetrics;
+import org.col.api.Name;
+import org.col.api.NameAct;
+import org.col.api.Reference;
+import org.col.api.Serial;
+import org.col.api.Taxon;
+import org.col.api.vocab.Country;
+import org.col.api.vocab.Extension;
+import org.col.api.vocab.Kingdom;
+import org.col.api.vocab.Language;
+import org.col.api.vocab.Lifezone;
+import org.col.api.vocab.NameIssue;
+import org.col.api.vocab.NamePart;
+import org.col.api.vocab.NameType;
+import org.col.api.vocab.NomenclaturalActType;
+import org.col.api.vocab.NomenclaturalCode;
+import org.col.api.vocab.NomenclaturalStatus;
+import org.col.api.vocab.Origin;
+import org.col.api.vocab.Rank;
+import org.col.api.vocab.TaxonIssue;
+import org.col.api.vocab.TaxonomicStatus;
+import org.col.api.vocab.TypeStatus;
 import org.col.commands.importer.neo.model.TaxonNameNode;
-import org.gbif.dwc.terms.*;
+import org.gbif.dwc.terms.AcTerm;
+import org.gbif.dwc.terms.DcElement;
+import org.gbif.dwc.terms.DcTerm;
+import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.dwc.terms.EolReferenceTerm;
+import org.gbif.dwc.terms.GbifInternalTerm;
+import org.gbif.dwc.terms.GbifTerm;
+import org.gbif.dwc.terms.IucnTerm;
+import org.gbif.dwc.terms.UnknownTerm;
+import org.gbif.dwc.terms.XmpRightsTerm;
+import org.gbif.dwc.terms.XmpTerm;
 import org.neo4j.kernel.impl.core.NodeProxy;
 
-import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.*;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.pool.KryoFactory;
+
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 
 /**
@@ -42,6 +81,7 @@ public class CliKryoFactory implements KryoFactory {
 
     // java & commons
     kryo.register(LocalDateTime.class);
+    kryo.register(LocalDate.class);
     kryo.register(HashMap.class);
     kryo.register(HashSet.class);
     kryo.register(ArrayList.class);
