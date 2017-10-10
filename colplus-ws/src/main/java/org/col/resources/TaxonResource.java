7bfa20e54b3183ddb4f4c6e8a1a12a9c04948057
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-10-10 12:57:41 +0200

    Added getVernacularNames to TaxonResource

--- colplus-ws/src/main/java/org/col/resources/TaxonResource.java
+++ colplus-ws/src/main/java/org/col/resources/TaxonResource.java
@@ -1,5 +1,6 @@
 package org.col.resources;
 
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,26 +11,38 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.ibatis.session.SqlSession;
 import org.col.api.Taxon;
+import org.col.api.VernacularName;
 import org.col.db.mapper.TaxonMapper;
+import org.col.db.mapper.VernacularNameMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.codahale.metrics.annotation.Timed;
 
-
 @Path("{datasetKey}/taxon")
 @Produces(MediaType.APPLICATION_JSON)
 public class TaxonResource {
 
-  @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private static final Logger LOG = LoggerFactory.getLogger(TaxonResource.class);
 
-  @GET
-  @Timed
-  @Path("{key}")
-  public Taxon get(@PathParam("datasetKey") Integer datasetKey, @PathParam("key") String key, @Context SqlSession session) {
-    TaxonMapper mapper = session.getMapper(TaxonMapper.class);
-    return mapper.get(datasetKey, key);
-  }
+	@GET
+	@Timed
+	@Path("{id}")
+	public Taxon get(@PathParam("datasetKey") Integer datasetKey, @PathParam("id") String id,
+	    @Context SqlSession session) {
+		TaxonMapper mapper = session.getMapper(TaxonMapper.class);
+		return mapper.get(datasetKey, id);
+	}
+
+	@GET
+	@Timed
+	@Path("{key}/getVernacularNames")
+	public List<VernacularName> getVernacularNames(
+	    @PathParam("datasetKey") Integer datasetKey, @PathParam("taxonKey") Integer taxonKey,
+	    @Context SqlSession session) {
+		VernacularNameMapper mapper = session.getMapper(VernacularNameMapper.class);
+		return mapper.getVernacularNamesForTaxon(datasetKey, taxonKey);
+	}
 
 }
