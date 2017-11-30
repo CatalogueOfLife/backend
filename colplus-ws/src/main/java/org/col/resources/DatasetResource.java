29c551f176db7ead2af617f3718d5196c76258ab
Ayco Holleman <ayco.holleman@naturalis.nl>
2017-11-30 15:39:50 +0100

    Dataset search done

--- colplus-ws/src/main/java/org/col/resources/DatasetResource.java
+++ colplus-ws/src/main/java/org/col/resources/DatasetResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
@@ -16,51 +17,62 @@ import org.apache.ibatis.session.SqlSession;
 import org.col.api.Dataset;
 import org.col.api.Page;
 import org.col.api.PagingResultSet;
+import org.col.dao.DatasetDao;
 import org.col.db.mapper.DatasetMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 @Path("/dataset")
 @Produces(MediaType.APPLICATION_JSON)
 public class DatasetResource {
-  private static final Logger LOG = LoggerFactory.getLogger(DatasetResource.class);
 
-  @GET
-  public PagingResultSet<Dataset> list(@Valid @BeanParam Page page, @Context SqlSession session) {
-    DatasetMapper mapper = session.getMapper(DatasetMapper.class);
-    return new PagingResultSet<Dataset>(page, mapper.count(), mapper.list(page));
-  }
+	@SuppressWarnings("unused")
+	private static final Logger LOG = LoggerFactory.getLogger(DatasetResource.class);
 
-  @POST
-  public Integer create(Dataset dataset, @Context SqlSession session) {
-    DatasetMapper mapper = session.getMapper(DatasetMapper.class);
-    mapper.create(dataset);
-    session.commit();
-    return dataset.getKey();
-  }
+	@GET
+	public PagingResultSet<Dataset> list(@Valid @BeanParam Page page, @Context SqlSession session) {
+		DatasetMapper mapper = session.getMapper(DatasetMapper.class);
+		return new PagingResultSet<Dataset>(page, mapper.count(), mapper.list(page));
+	}
 
-  @GET
-  @Path("{key}")
-  public Dataset get(@PathParam("key") Integer key, @Context SqlSession session) {
-    DatasetMapper mapper = session.getMapper(DatasetMapper.class);
-    return mapper.get(key);
-  }
+	@GET
+	@Path("/search")
+	public PagingResultSet<Dataset> search(@QueryParam("q") String q,
+	    @Valid @BeanParam Page page,
+	    @Context SqlSession session) {
+		DatasetDao dao = new DatasetDao(session);
+		return dao.search(q, page);
+	}
 
-  @PUT
-  @Path("{key}")
-  public void update(Dataset dataset, @Context SqlSession session) {
-    DatasetMapper mapper = session.getMapper(DatasetMapper.class);
-    mapper.update(dataset);
-    session.commit();
-  }
+	@POST
+	public Integer create(Dataset dataset, @Context SqlSession session) {
+		DatasetMapper mapper = session.getMapper(DatasetMapper.class);
+		mapper.create(dataset);
+		session.commit();
+		return dataset.getKey();
+	}
 
-  @DELETE
-  @Path("{key}")
-  public void delete(@PathParam("key") Integer key, @Context SqlSession session) {
-    DatasetMapper mapper = session.getMapper(DatasetMapper.class);
-    mapper.delete(key);
-    session.commit();
-  }
+	@GET
+	@Path("{key}")
+	public Dataset get(@PathParam("key") Integer key, @Context SqlSession session) {
+		DatasetMapper mapper = session.getMapper(DatasetMapper.class);
+		return mapper.get(key);
+	}
+
+	@PUT
+	@Path("{key}")
+	public void update(Dataset dataset, @Context SqlSession session) {
+		DatasetMapper mapper = session.getMapper(DatasetMapper.class);
+		mapper.update(dataset);
+		session.commit();
+	}
+
+	@DELETE
+	@Path("{key}")
+	public void delete(@PathParam("key") Integer key, @Context SqlSession session) {
+		DatasetMapper mapper = session.getMapper(DatasetMapper.class);
+		mapper.delete(key);
+		session.commit();
+	}
 
 }
