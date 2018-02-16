e53b07f58cbd698f244d4ef895151347a8e12eeb
mdoering <mdoering@gbif.org>
2018-02-16 20:05:24 +0100

    Fix format in sql

--- colplus-api/src/main/java/org/col/api/vocab/DataFormat.java
+++ colplus-api/src/main/java/org/col/api/vocab/DataFormat.java
@@ -4,5 +4,20 @@ package org.col.api.vocab;
  * Various external data formats supported by the application
  */
 public enum DataFormat {
-  DWCA, ACEF, TCS
+
+  /**
+   * Darwin Core Archive
+   */
+  DWCA,
+
+  /**
+   * CoL Data Submission Format ("Annual Checklist Exchange Format")
+   * http://www.catalogueoflife.org/content/contributing-your-data#ACEF
+   */
+  ACEF,
+
+  /**
+   * TDWG Taxonomic Concept Schema
+   */
+  TCS
 }
