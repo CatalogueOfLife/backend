257e77149a42b92dedd0b1f57fb5ade8c2e6413a
mdoering <mdoering@gbif.org>
2018-05-17 16:44:41 +0200

    Remove bad unecessary imports

--- colplus-common/src/main/java/org/col/common/util/YamlUtils.java
+++ colplus-common/src/main/java/org/col/common/util/YamlUtils.java
@@ -3,15 +3,11 @@ package org.col.common.util;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.Charset;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.google.common.base.Charsets;
-import com.sun.xml.internal.ws.util.StreamUtils;
-import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -28,7 +24,6 @@ public class YamlUtils {
    */
   public static <T> T read(Class<T> objClass, String resourceFile) throws IOException {
     InputStream in = System.class.getResourceAsStream(resourceFile);
-    //System.out.println(IOUtils.toString(in, Charsets.UTF_8));
     return OM.readValue(in, objClass);
   }
 
