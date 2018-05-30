19c160185d5a636be7563c4cd7c17c20c21fbd0e
mdoering <mdoering@gbif.org>
2018-05-30 14:20:24 +0200

    Warn if import queue is too small

--- colplus-admin/src/main/java/org/col/admin/importer/ContinousImporter.java
+++ colplus-admin/src/main/java/org/col/admin/importer/ContinousImporter.java
@@ -36,6 +36,10 @@ public class ContinousImporter implements Managed {
 
   public ContinousImporter(ImporterConfig cfg, ImportManager manager, SqlSessionFactory factory) {
     this.job = new ContinousImporterJob(cfg, manager, factory);
+    if (cfg.maxQueue <= BATCH_SIZE) {
+      job.running=false;
+      LOG.warn("Importer queue is shorter ({}) than the amount of batches ({}) to submit. Shutdown continous importer!", cfg.maxQueue, BATCH_SIZE);
+    }
   }
 
   static class ContinousImporterJob implements Runnable {
