0c7ded57edb3262842b6d3e4ec6a68c53ceb13f5
mdoering <mdoering@gbif.org>
2018-09-04 12:24:02 +0200

    Use embedded postgres method to generate cached server configs

--- colplus-dao/src/test/java/org/col/db/EmbeddedColPg.java
+++ colplus-dao/src/test/java/org/col/db/EmbeddedColPg.java
@@ -10,18 +10,11 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Random;
 
-import de.flapdoodle.embed.process.config.IRuntimeConfig;
-import de.flapdoodle.embed.process.io.directories.FixedPath;
-import de.flapdoodle.embed.process.store.PostgresArtifactStoreBuilder;
 import org.apache.commons.io.FileUtils;
 import org.col.common.lang.Exceptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.yandex.qatools.embed.postgresql.Command;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
-import ru.yandex.qatools.embed.postgresql.PackagePaths;
-import ru.yandex.qatools.embed.postgresql.config.PostgresDownloadConfigBuilder;
-import ru.yandex.qatools.embed.postgresql.config.RuntimeConfigBuilder;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
 import static java.util.Arrays.asList;
@@ -71,39 +64,25 @@ public class EmbeddedColPg {
 		}
 	}
 
-	private IRuntimeConfig buildRuntimeConfig() throws IOException {
-		// turns off the default functionality of unzipping on every run.
-		tmpServerDir = cfg.host == null;
-		serverDir = tmpServerDir ? Files.createTempDirectory("colplus-pg-") : Paths.get(cfg.host);
-		LOG.info("Embedded Postgres cached server dir: {}", serverDir);
-		final Command cmd = Command.Postgres;
-		final FixedPath cachedDir = new FixedPath(serverDir.toString());
-		return new RuntimeConfigBuilder()
-				.defaults(cmd)
-				.daemonProcess(false)
-				.artifactStore(new PostgresArtifactStoreBuilder()
-						.defaults(cmd)
-						.tempDir(cachedDir)
-						.download(new PostgresDownloadConfigBuilder()
-								.defaultsForCommand(cmd)
-								.packageResolver(new PackagePaths(cmd, cachedDir))
-								.build()))
-				.build();
-	}
-
 	private void startDb() {
 		try {
 			LOG.info("Starting embedded Postgres");
 			Instant start = Instant.now();
-			IRuntimeConfig rtCfg = buildRuntimeConfig();
+			// use a cached server directory to not unzip postgres binaries on every run
+			tmpServerDir = cfg.host == null;
+			serverDir = tmpServerDir ? Files.createTempDirectory("colplus-pg-") : Paths.get(cfg.host);
 			dataDir = serverDir.resolve("data/"+String.valueOf(new Random().nextInt()));
 			Files.createDirectories(dataDir);
+			LOG.debug("Use embedded Postgres, server dir={}, data dir={}", serverDir, dataDir);
 			postgres = new EmbeddedPostgres(Version.V10_3, dataDir.toString());
 			// assigned some free port using local socket 0
 			cfg.port = new ServerSocket(0).getLocalPort();
 			cfg.host = "localhost";
-			cfg.maximumPoolSize = 2;
-			postgres.start(rtCfg, cfg.host, cfg.port, cfg.database, cfg.user, cfg.password, DEFAULT_ADD_PARAMS);
+			cfg.maximumPoolSize = 4;
+			postgres.start(EmbeddedPostgres.cachedRuntimeConfig(serverDir),
+					cfg.host, cfg.port, cfg.database, cfg.user, cfg.password,
+					DEFAULT_ADD_PARAMS
+			);
 			if (postgres.getProcess().isPresent()) {
 				LOG.info("Pg started on port {}. Startup time: {} ms", cfg.port, Duration.between(start, Instant.now()).toMillis());
 			} else {
