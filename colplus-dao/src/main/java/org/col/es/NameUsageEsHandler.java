b1a63cea0c5ac7b5a614b9f098bc096c7008469f
Ayco Holleman <ayco.holleman@naturalis.nl>
2018-09-06 15:48:38 +0200

    stub class NameUsageEsHandler

---
+++ colplus-dao/src/main/java/org/col/es/NameUsageEsHandler.java
@@ -0,0 +1,14 @@
+package org.col.es;
+
+import org.apache.ibatis.session.ResultContext;
+import org.apache.ibatis.session.ResultHandler;
+import org.col.api.model.NameUsage;
+
+public class NameUsageEsHandler implements ResultHandler<NameUsage> {
+
+  @Override
+  public void handleResult(ResultContext<? extends NameUsage> resultContext) {
+    
+  }
+
+}
