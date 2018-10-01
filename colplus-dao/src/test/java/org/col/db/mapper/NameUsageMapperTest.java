e99978b9a05837a83210e7a8c37302ced4f16c66
mdoering <mdoering@gbif.org>
2018-10-01 16:36:20 +0200

    Map vernaculars in TaxonVernacularUsage

--- colplus-dao/src/test/java/org/col/db/mapper/NameUsageMapperTest.java
+++ colplus-dao/src/test/java/org/col/db/mapper/NameUsageMapperTest.java
@@ -2,13 +2,18 @@ package org.col.db.mapper;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.ibatis.session.ResultContext;
+import org.apache.ibatis.session.ResultHandler;
 import org.col.api.model.BareName;
 import org.col.api.model.Synonym;
 import org.col.api.model.TaxonVernacularUsage;
+import org.col.api.model.VernacularName;
 import org.junit.Assert;
 import org.junit.Test;
 
 import static org.col.api.TestEntityGenerator.NAME4;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 
 public class NameUsageMapperTest extends MapperTestBase<NameUsageMapper> {
@@ -20,34 +25,37 @@ public class NameUsageMapperTest extends MapperTestBase<NameUsageMapper> {
   
   @Test
   public void processDatasetTaxa() throws Exception {
-    try (BatchResultHandler<TaxonVernacularUsage> handler = new BatchResultHandler<TaxonVernacularUsage>(
-        batch -> counter.addAndGet(batch.size()
-        ), 10)
-    ) {
-      mapper().processDatasetTaxa(NAME4.getDatasetKey(), handler);
-    }
+    mapper().processDatasetTaxa(NAME4.getDatasetKey(), new ResultHandler<TaxonVernacularUsage>() {
+      public void handleResult(ResultContext<? extends TaxonVernacularUsage> ctx) {
+        counter.incrementAndGet();
+        for (VernacularName v : ctx.getResultObject().getVernacularNames()) {
+          assertNotNull(v.getName());
+        }
+      }
+    });
     Assert.assertEquals(2, counter.get());
   }
   
   @Test
   public void processDatasetSynonyms() throws Exception {
-    try (BatchResultHandler<Synonym> handler = new BatchResultHandler<Synonym>(
-        batch -> counter.addAndGet(batch.size()
-        ), 10)
-    ) {
-      mapper().processDatasetSynonyms(NAME4.getDatasetKey(), handler);
-    }
+    mapper().processDatasetSynonyms(NAME4.getDatasetKey(), new ResultHandler<Synonym>() {
+      public void handleResult(ResultContext<? extends Synonym> ctx) {
+        counter.incrementAndGet();
+        assertTrue(ctx.getResultObject().getStatus().isSynonym());
+        assertNotNull(ctx.getResultObject().getAccepted());
+      }
+    });
     Assert.assertEquals(2, counter.get());
   }
   
   @Test
   public void processDatasetBareNames() throws Exception {
-    try (BatchResultHandler<BareName> handler = new BatchResultHandler<BareName>(
-        batch -> counter.addAndGet(batch.size()
-        ), 10)
-    ) {
-      mapper().processDatasetBareNames(NAME4.getDatasetKey(), handler);
-    }
+    mapper().processDatasetBareNames(NAME4.getDatasetKey(), new ResultHandler<BareName>() {
+      public void handleResult(ResultContext<? extends BareName> ctx) {
+        counter.incrementAndGet();
+        assertNotNull(ctx.getResultObject());
+      }
+    });
     Assert.assertEquals(0, counter.get());
   }
 }
