5230f23f7015268d22fe65f60f4798886124e1ca
mdoering <mdoering@gbif.org>
2018-10-29 21:27:51 +0100

    Test sectors in tree

--- colplus-dao/src/test/java/org/col/db/mapper/MapperTestBase.java
+++ colplus-dao/src/test/java/org/col/db/mapper/MapperTestBase.java
@@ -2,12 +2,18 @@ package org.col.db.mapper;
 
 import java.time.LocalDateTime;
 
+import org.col.api.TestEntityGenerator;
 import org.col.api.model.DatasetImport;
+import org.col.api.model.Name;
+import org.col.api.model.Taxon;
 import org.col.api.vocab.ImportState;
 import org.col.db.PgSetupRule;
 import org.col.db.dao.DatasetImportDao;
+import org.gbif.nameparser.api.Rank;
 import org.junit.ClassRule;
 import org.junit.Rule;
+
+import static org.col.api.vocab.Datasets.DRAFT_CAT;
 
 /**
  * A reusable base class for all mybatis mapper tests that takes care of postgres & mybatis.
@@ -39,8 +45,8 @@ public abstract class MapperTestBase<T> {
   public void commit() {
     initMybatisRule.commit();
   }
-
-  public void generateDatasetImport(int datasetKey) {
+  
+  protected void generateDatasetImport(int datasetKey) {
     DatasetImportMapper dim = mapper(DatasetImportMapper.class);
 
     DatasetImport d = new DatasetImport();
@@ -55,5 +61,53 @@ public abstract class MapperTestBase<T> {
     dao.updateMetrics(dim, d);
     dim.update(d);
     commit();
+  }
+  
+  protected void partition(int datasetKey) {
+    final DatasetPartitionMapper pm = mapper(DatasetPartitionMapper.class);
+    pm.delete(datasetKey);
+    pm.create(datasetKey);
+    pm.attach(datasetKey);
+    commit();
+  }
+  
+  
+  protected void populateDraftTree() {
+    partition(DRAFT_CAT);
+    
+    NameMapper nm = mapper(NameMapper.class);
+  
+    Name n1 = draftName(nm,"n1", "Animalia", Rank.KINGDOM);
+    Name n2 = draftName(nm,"n2", "Arthropoda", Rank.KINGDOM);
+    Name n3 = draftName(nm,"n3", "Insecta", Rank.CLASS);
+    Name n4 = draftName(nm,"n4", "Coleoptera", Rank.ORDER);
+    Name n5 = draftName(nm,"n5", "Lepidoptera", Rank.ORDER);
+    
+    TaxonMapper tm = mapper(TaxonMapper.class);
+    Taxon t1 = draftTaxon(tm,"t1", n1, null);
+    Taxon t2 = draftTaxon(tm,"t2", n2, t1);
+    Taxon t3 = draftTaxon(tm,"t3", n3, t2);
+    Taxon t4 = draftTaxon(tm,"t4", n4, t3);
+    Taxon t5 = draftTaxon(tm,"t5", n5, t3);
+    
+    commit();
+  }
+  
+  private static Name draftName(NameMapper nm, String id, String name, Rank rank) {
+    Name n = TestEntityGenerator.newName(DRAFT_CAT,id, name, rank);
+    nm.create(n);
+    return n;
+  }
+  
+  private static Taxon draftTaxon(TaxonMapper tm, String id, Name n, Taxon parent) {
+    Taxon t = TestEntityGenerator.newTaxon(DRAFT_CAT, id);
+    t.setName(n);
+    if (parent == null) {
+      t.setParentId(null);
+    } else {
+      t.setParentId(parent.getId());
+    }
+    tm.create(t);
+    return t;
   }
 }
