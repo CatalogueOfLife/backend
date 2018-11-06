c8177a16924281c857b262f19e6e04fcca0d569d
mdoering <mdoering@gbif.org>
2018-11-06 19:57:28 +0100

    Add default language header

--- colplus-dw/src/main/java/org/col/dw/cors/CorsConfiguration.java
+++ colplus-dw/src/main/java/org/col/dw/cors/CorsConfiguration.java
@@ -12,7 +12,7 @@ public class CorsConfiguration {
   public String methods = "OPTIONS, HEAD, GET, POST, PUT, DELETE";
   
   @NotEmpty
-  public String headers = "Authorization, Content-Type";
+  public String headers = "Authorization, Content-Type, Accept-Language";
 
   public boolean anyOrigin() {
     return ANY_ORIGIN.equals(origins);
