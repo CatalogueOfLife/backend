70b5828f9bdd6144d380eb49a079454f2ff98f5c
mdoering <mdoering@gbif.org>
2018-11-08 11:19:09 +0100

    Add BatchResultHandler javadocs

--- colplus-dao/src/main/java/org/col/db/mapper/BatchResultHandler.java
+++ colplus-dao/src/main/java/org/col/db/mapper/BatchResultHandler.java
@@ -8,6 +8,10 @@ import com.google.common.base.Preconditions;
 import org.apache.ibatis.session.ResultContext;
 import org.apache.ibatis.session.ResultHandler;
 
+/**
+ * Mybatis result handler that offers the streamed records in batches.
+ * Make always sure to close the handler to submit the last batch!
+ */
 public class BatchResultHandler<T> implements ResultHandler<T>, AutoCloseable {
   
   private final Consumer<List<T>> batchConsumer;
