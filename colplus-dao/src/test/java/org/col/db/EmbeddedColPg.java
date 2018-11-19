0c66e820e43a0466c352d5607f86c6689c59c4ad
mdoering <mdoering@gbif.org>
2018-11-19 14:25:11 +0100

    Use Postgres 11 for tests

--- colplus-dao/src/test/java/org/col/db/EmbeddedColPg.java
+++ colplus-dao/src/test/java/org/col/db/EmbeddedColPg.java
@@ -16,7 +16,6 @@ import org.col.common.lang.Exceptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
-import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
 import static java.util.Arrays.asList;
 
@@ -28,7 +27,7 @@ import static java.util.Arrays.asList;
 public class EmbeddedColPg {
   private static final Logger LOG = LoggerFactory.getLogger(EmbeddedColPg.class);
 	
-	private static final String VERSION = Version.V10_3.asInDownloadPath(); //"11.0-1";
+	private static final String VERSION = "11.0-1";
 	private static final List<String> DEFAULT_ADD_PARAMS = asList(
 			"-E", "SQL_ASCII",
 			"--locale=C",
