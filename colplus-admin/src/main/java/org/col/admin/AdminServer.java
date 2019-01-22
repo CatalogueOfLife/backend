6bfec087cc5231c54d0496633c700a3eb5305f3e
Markus Döring <m.doering@mac.com>
2019-01-22 15:10:30 +0100

    Manage assembly coordinator, fixes #250

--- colplus-admin/src/main/java/org/col/admin/AdminServer.java
+++ colplus-admin/src/main/java/org/col/admin/AdminServer.java
@@ -102,6 +102,7 @@ public class AdminServer extends PgApp<AdminServerConfig> {
   
     // assembly
     AssemblyCoordinator assembly = new AssemblyCoordinator(getSqlSessionFactory(), env.metrics());
+    env.lifecycle().manage(assembly);     
     env.jersey().register(new AssemblyResource(assembly));
     
   }
