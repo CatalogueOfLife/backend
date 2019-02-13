680a9e5acb9f1f609093363014e1d129ef97e61b
Geoff Ower <gdower@illinois.edu>
2019-02-13 09:25:51 -0600

    Added Early Land Plants Today (ELPT)

--- colplus-dao/src/main/resources/org/col/db/datasets.sql
+++ colplus-dao/src/main/resources/org/col/db/datasets.sql
@@ -310,6 +310,7 @@ WHERE gbif_key IS NOT NULL;
 INSERT INTO dataset (key, origin, type, contributes_to, code, title, import_frequency, created_by, modified_by, data_format, data_access) VALUES
 ('1027', 0, 1, 0,    4, 'Scarabs',           1, 0, 0, 1, 'https://github.com/Sp2000/data-scarabs/archive/master.zip'),
 ('1055', 0, 1, 0,    4, 'Neuropterida',      1, 0, 0, 1, 'https://github.com/Sp2000/data-neuropterida/archive/master.zip'),
+('1074', 0, 1, 0,    1, 'ELPT',              1, 0, 0, 1, 'https://github.com/Sp2000/data-elpt/archive/master.zip'),
 ('1140', 0, 0, 0,    1, 'WorldFerns',        1, 0, 0, 1, 'https://github.com/Sp2000/data-world-ferns/archive/master.zip'),
 ('1141', 0, 0, 0,    1, 'WorldPlants',       1, 0, 0, 0, 'https://github.com/Sp2000/data-world-plants/archive/master.zip'),
 ('1202', 0, 1, 0,    4, 'WoRMS Amphipoda',   1, 0, 0, 1, 'https://raw.githubusercontent.com/Sp2000/colplus-repo/master/ACEF/202.tar.gz'),
