3d5d2b9cf773ab9f4d1004bbc8a58b69d7ee91e0
Markus Döring <m.doering@mac.com>
2019-02-19 14:10:21 +0100

    Start integrity checks

---
+++ colplus-admin/src/test/java/org/col/admin/importer/IntegrityChecksIT.java
@@ -0,0 +1,138 @@
+package org.col.admin.importer;
+
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.col.admin.command.initdb.InitDbCmd;
+import org.col.admin.config.ImporterConfig;
+import org.col.admin.config.NormalizerConfig;
+import org.col.admin.importer.neo.NeoDb;
+import org.col.admin.importer.neo.NeoDbFactory;
+import org.col.admin.matching.NameIndexFactory;
+import org.col.api.model.*;
+import org.col.api.vocab.*;
+import org.col.db.PgSetupRule;
+import org.col.db.dao.DatasetImportDao;
+import org.col.db.dao.TaxonDao;
+import org.col.db.mapper.DatasetMapper;
+import org.col.db.mapper.InitMybatisRule;
+import org.col.db.mapper.VerbatimRecordMapper;
+import org.gbif.nameparser.api.Rank;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Checks for unit test ColDP archives under resources/integrity-checks/
+ */
+public class IntegrityChecksIT {
+  
+  private NeoDb store;
+  private NormalizerConfig cfg;
+  private ImporterConfig icfg = new ImporterConfig();
+  private Dataset dataset;
+  private VerbatimRecordMapper vMapper;
+  
+  @ClassRule
+  public static PgSetupRule pgSetupRule = new PgSetupRule();
+  
+  @Rule
+  public InitMybatisRule initMybatisRule = InitMybatisRule.empty();
+  
+  @Before
+  public void initCfg() throws Exception {
+    cfg = new NormalizerConfig();
+    cfg.archiveDir = Files.createTempDir();
+    cfg.scratchDir = Files.createTempDir();
+    dataset = new Dataset();
+    dataset.setContributesTo(Catalogue.PCAT);
+    dataset.setCreatedBy(InitMybatisRule.TEST_USER.getKey());
+    dataset.setModifiedBy(InitMybatisRule.TEST_USER.getKey());
+
+    InitDbCmd.setupStandardPartitions(initMybatisRule.getSqlSession());
+    initMybatisRule.commit();
+ }
+  
+  @After
+  public void cleanup() throws Exception {
+    if (store != null) {
+      store.closeAndDelete();
+      FileUtils.deleteQuietly(cfg.archiveDir);
+      FileUtils.deleteQuietly(cfg.scratchDir);
+    }
+  }
+  
+  void normalizeAndImport(String key) throws Exception {
+    URL url = getClass().getResource("/integrity-checks/" + key);
+    dataset.setDataFormat(DataFormat.COLDP);
+    try {
+      // insert trusted dataset
+      dataset.setTitle("Integrity check " + key);
+      
+      SqlSession session = PgSetupRule.getSqlSessionFactory().openSession(true);
+      // this creates a new key, usually 2000!
+      session.getMapper(DatasetMapper.class).create(dataset);
+      session.commit();
+      session.close();
+      
+      // normalize
+      store = NeoDbFactory.create(dataset.getKey(), cfg);
+      store.put(dataset);
+      Normalizer norm = new Normalizer(store, Paths.get(url.toURI()), NameIndexFactory.memory(Datasets.PCAT, PgSetupRule.getSqlSessionFactory()));
+      norm.call();
+      
+      // import into postgres
+      store = NeoDbFactory.open(dataset.getKey(), cfg);
+      PgImport importer = new PgImport(dataset.getKey(), store, PgSetupRule.getSqlSessionFactory(), icfg);
+      importer.call();
+      
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  DatasetImport metrics() {
+    return new DatasetImportDao(PgSetupRule.getSqlSessionFactory()).generateMetrics(dataset.getKey());
+  }
+  
+  
+  @Test
+  @Ignore("depends on duplicate detection working")
+  public void testA1() throws Exception {
+    normalizeAndImport("A1");
+    
+    try (SqlSession session = PgSetupRule.getSqlSessionFactory().openSession(true)) {
+      TaxonDao tdao = new TaxonDao(session);
+      
+      Taxon t = tdao.get(dataset.getKey(), "14649");
+      assertEquals("Zapoteca formosa", t.getName().getScientificName());
+      assertEquals("(Kunth) H.M.Hern.", t.getName().authorshipComplete());
+      assertEquals(Rank.SPECIES, t.getName().getRank());
+      
+      TaxonInfo info = tdao.getTaxonInfo(t);
+      // distributions
+      assertEquals(3, info.getDistributions().size());
+      Set<String> areas = Sets.newHashSet("AGE-BA", "BZC-MS", "BZC-MT");
+      for (Distribution d : info.getDistributions()) {
+        assertEquals(Gazetteer.TDWG, d.getGazetteer());
+        assertTrue(areas.remove(d.getArea()));
+      }
+      
+      // vernacular
+      assertEquals(3, info.getVernacularNames().size());
+      Set<String> names = Sets.newHashSet("Ramkurthi", "Ram Kurthi", "отчество");
+      for (VernacularName v : info.getVernacularNames()) {
+        assertEquals(v.getName().startsWith("R") ? Language.HINDI : Language.RUSSIAN,
+            v.getLanguage());
+        assertTrue(names.remove(v.getName()));
+      }
+    }
+  }
+  
+}
