8968df4f23e74215e00460d31e69872b09b3c304
Markus Döring <m.doering@mac.com>
2019-02-25 14:09:40 +0100

    Forgot to checkin StringSetTypeHandler

---
+++ colplus-dao/src/main/java/org/col/db/type/StringSetTypeHandler.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013 Global Biodiversity Information Facility (GBIF)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copyTaxon of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.col.db.type;
+
+import java.sql.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+/**
+ * Stores string sets as non null arrays in postgres, avoiding nulls and uses empty sets instead.
+ */
+public class StringSetTypeHandler extends BaseTypeHandler<Set<String>> {
+  
+  @Override
+  public void setNonNullParameter(PreparedStatement ps, int i, Set<String> parameter, JdbcType jdbcType) throws SQLException {
+    Array array = ps.getConnection().createArrayOf("text", parameter.toArray());
+    ps.setArray(i, array);
+  }
+  
+  @Override
+  public void setParameter(PreparedStatement ps, int i, Set<String> parameter, JdbcType jdbcType) throws SQLException {
+    setNonNullParameter(ps, i, parameter == null ? Collections.emptySet() : parameter, jdbcType);
+  }
+  
+  @Override
+  public Set<String> getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    return toSet(rs.getArray(columnName));
+  }
+  
+  @Override
+  public Set<String> getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    return toSet(rs.getArray(columnIndex));
+  }
+  
+  @Override
+  public Set<String> getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    return toSet(cs.getArray(columnIndex));
+  }
+  
+  private Set<String> toSet(Array pgArray) throws SQLException {
+    if (pgArray == null) return new HashSet<>();
+    
+    String[] strings = (String[]) pgArray.getArray();
+    return Sets.newHashSet(strings);
+  }
+}
