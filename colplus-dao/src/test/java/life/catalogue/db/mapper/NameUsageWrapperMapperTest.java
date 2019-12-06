95f40424adb2846454bb882169841e3b75f73410
Markus Döring <m.doering@mac.com>
2019-12-06 15:12:55 +0100

    Fix sectorDatasetKey in ES

--- colplus-dao/src/test/java/life/catalogue/db/mapper/NameUsageWrapperMapperTest.java
+++ colplus-dao/src/test/java/life/catalogue/db/mapper/NameUsageWrapperMapperTest.java
@@ -1,14 +1,22 @@
 package life.catalogue.db.mapper;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
+import life.catalogue.api.TestEntityGenerator;
+import life.catalogue.api.model.Dataset;
+import life.catalogue.api.model.Sector;
+import life.catalogue.api.model.Taxon;
+import life.catalogue.api.search.NameUsageWrapper;
+import life.catalogue.api.vocab.Datasets;
 import org.apache.ibatis.session.ResultContext;
 import org.apache.ibatis.session.ResultHandler;
-import life.catalogue.api.search.NameUsageWrapper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static life.catalogue.api.TestEntityGenerator.NAME4;
+import static life.catalogue.api.TestEntityGenerator.TAXON2;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
@@ -31,5 +39,53 @@ public class NameUsageWrapperMapperTest extends MapperTestBase<NameUsageWrapperM
       }
     });
     Assert.assertEquals(1, counter.get());
+  }
+
+
+  @Test
+  public void testGetComplete() throws Exception {
+    DatasetMapper dm = mapper(DatasetMapper.class);
+    Dataset d = dm.get(Datasets.DRAFT_COL);
+    d.setGbifPublisherKey(UUID.randomUUID());
+    dm.update(d);
+
+    Taxon t = TestEntityGenerator.newTaxon(Datasets.DRAFT_COL);
+    NameMapper nm = mapper(NameMapper.class);
+    nm.create(t.getName());
+
+    TaxonMapper tm = mapper(TaxonMapper.class);
+    tm.create(t);
+
+    SectorMapper sm = mapper(SectorMapper.class);
+    Sector s = new Sector();
+    s.setSubjectDatasetKey(TAXON2.getDatasetKey());
+    s.setSubject(TAXON2.toSimpleName());
+    s.setDatasetKey(d.getKey());
+    s.setTarget(t.toSimpleName());
+    s.applyUser(TestEntityGenerator.USER_USER);
+    sm.create(s);
+
+    t.setSectorKey(s.getKey());
+    tm.update(t);
+
+    commit();
+
+    NameUsageWrapper w = mapper().get(Datasets.DRAFT_COL, t.getId());
+    assertNotNull(w);
+    assertNotNull(w.getUsage());
+    Taxon wt = (Taxon) w.getUsage();
+    assertNotNull(wt.getName());
+    assertEquals(s.getKey(), wt.getSectorKey());
+    assertEquals(TAXON2.getDatasetKey(), w.getSectorDatasetKey());
+    assertEquals(d.getGbifPublisherKey(), w.getPublisherKey());
+
+    w = mapper().getWithoutClassification(Datasets.DRAFT_COL, t.getId());
+    assertNotNull(w);
+    assertNotNull(w.getUsage());
+    wt = (Taxon) w.getUsage();
+    assertNotNull(wt.getName());
+    assertEquals(s.getKey(), wt.getSectorKey());
+    assertEquals(TAXON2.getDatasetKey(), w.getSectorDatasetKey());
+    assertEquals(d.getGbifPublisherKey(), w.getPublisherKey());
   }
 }
