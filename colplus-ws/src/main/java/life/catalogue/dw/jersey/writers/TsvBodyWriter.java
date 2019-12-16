934ed5da6e9221d90965c0b181005f452beae112
Markus Döring <m.doering@mac.com>
2019-12-16 21:06:22 +0100

    Add simple C/TSV export for dataset name usages

---
+++ colplus-ws/src/main/java/life/catalogue/dw/jersey/writers/TsvBodyWriter.java
@@ -0,0 +1,56 @@
+package life.catalogue.dw.jersey.writers;
+
+import com.google.common.base.Throwables;
+import life.catalogue.common.io.UTF8IOUtils;
+import life.catalogue.dw.jersey.MoreMediaTypes;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.stream.Stream;
+
+@Produces({MoreMediaTypes.TEXT_TSV})
+@Provider
+public class TsvBodyWriter implements MessageBodyWriter<Stream<Object[]>> {
+  
+  @Override
+  public boolean isWriteable(Class<?> type, Type type1, Annotation[] antns, MediaType mt) {
+    return Stream.class.isAssignableFrom(type);
+  }
+  
+  @Override
+  public void writeTo(Stream<Object[]> rows, Class<?> type, Type type1, Annotation[] antns, MediaType mt, MultivaluedMap<String, Object> mm, OutputStream out) throws IOException, WebApplicationException {
+    BufferedWriter br = UTF8IOUtils.writerFromStream(out);
+    try {
+      rows.forEach(row -> {
+        try {
+          br.append(toTsv(row));
+        } catch(Exception e) {
+          Throwables.throwIfUnchecked(e);
+          throw new RuntimeException(e);
+        }
+      });
+    } finally {
+      br.close();
+    }
+  }
+
+  private static String toTsv(Object[] row) {
+    StringBuilder sb = new StringBuilder();
+    for (Object col : row) {
+      if (sb.length() > 0) {
+        sb.append('\t');
+      }
+      sb.append(col.toString().replaceAll("\t", " "));
+    }
+    return sb.toString();
+  }
+}
--- colplus-ws/src/main/java/life/catalogue/resources/NameUsageDatasetSearchResource.java
+++ colplus-ws/src/main/java/life/catalogue/resources/NameUsageDatasetSearchResource.java
@@ -1,20 +1,27 @@
 package life.catalogue.resources;
 
+import com.codahale.metrics.annotation.Timed;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Streams;
+import life.catalogue.api.model.NameUsageBase;
+import life.catalogue.api.model.Page;
+import life.catalogue.api.model.ResultPage;
+import life.catalogue.api.search.*;
+import life.catalogue.db.mapper.NameUsageWrapperMapper;
+import life.catalogue.dw.jersey.MoreMediaTypes;
+import life.catalogue.es.InvalidQueryException;
+import life.catalogue.es.name.search.NameUsageSearchService;
+import life.catalogue.es.name.suggest.NameUsageSuggestionService;
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
-
-import com.codahale.metrics.annotation.Timed;
-import life.catalogue.api.model.Page;
-import life.catalogue.api.model.ResultPage;
-import life.catalogue.api.search.*;
-import life.catalogue.es.InvalidQueryException;
-import life.catalogue.es.name.search.NameUsageSearchService;
-import life.catalogue.es.name.suggest.NameUsageSuggestionService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.stream.Stream;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/dataset/{datasetKey}/nameusage")
@@ -22,13 +29,38 @@ public class NameUsageDatasetSearchResource {
 
   @SuppressWarnings("unused")
   private static final Logger LOG = LoggerFactory.getLogger(NameUsageDatasetSearchResource.class);
-
+  private static final Joiner COMMA_CAT = Joiner.on(',').skipNulls();
+  private static final Object[][] NAME_HEADER = new Object[1][];
+  static {
+    NAME_HEADER[0] = new Object[]{"ID", "parentID", "status", "rank", "scientificName", "authorship", "issues"};
+  }
   private final NameUsageSearchService searchService;
   private final NameUsageSuggestionService suggestService;
 
   public NameUsageDatasetSearchResource(NameUsageSearchService search, NameUsageSuggestionService suggest) {
     this.searchService = search;
     this.suggestService = suggest;
+  }
+
+  @GET
+  @Produces({MoreMediaTypes.TEXT_CSV, MoreMediaTypes.TEXT_TSV})
+  public Stream<Object[]> exportCsv(@PathParam("datasetKey") int datasetKey,
+                                  @QueryParam("issue") boolean withIssueOnly,
+                                  @Context SqlSession session) {
+    NameUsageWrapperMapper nuwm = session.getMapper(NameUsageWrapperMapper.class);
+    return Stream.concat(
+            Stream.of(NAME_HEADER),
+            Streams.stream(nuwm.processDatasetUsageOnly(datasetKey, withIssueOnly))
+              .map(nu -> new Object[]{
+                  nu.getId(),
+                  ((NameUsageBase) nu.getUsage()).getParentId(),
+                  nu.getUsage().getStatus(),
+                  nu.getUsage().getName().getRank(),
+                  nu.getUsage().getName().getScientificName(),
+                  nu.getUsage().getName().getAuthorship(),
+                  COMMA_CAT.join(nu.getIssues())
+            })
+    );
   }
 
   @GET
