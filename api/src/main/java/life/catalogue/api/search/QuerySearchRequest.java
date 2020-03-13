3ff78dd36f73e1e9a84591c23c99aec9d032b88c
Markus Döring <m.doering@mac.com>
2020-03-13 14:22:54 +0100

    Refactor poms to use org.catalogueoflife and remove colplus

---
+++ api/src/main/java/life/catalogue/api/search/QuerySearchRequest.java
@@ -0,0 +1,28 @@
+package life.catalogue.api.search;
+
+import javax.ws.rs.QueryParam;
+
+public class QuerySearchRequest {
+  
+  @QueryParam("q")
+  private String query;
+
+  public QuerySearchRequest() {
+  }
+
+  public QuerySearchRequest(String query) {
+    this.query = query;
+  }
+
+  public String getQ() {
+    return query;
+  }
+
+  public String getQuery() {
+    return query;
+  }
+
+  public void setQuery(String query) {
+    this.query = query;
+  }
+}
