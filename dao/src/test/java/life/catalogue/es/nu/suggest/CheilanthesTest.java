872fd7e67073ab899cd53d01c1abd0c9d863d3ea
Ayco Holleman <ayco.holleman@naturalis.nl>
2020-07-01 09:18:57 +0200

    Tests for cheilanthess pass

--- dao/src/test/java/life/catalogue/es/nu/suggest/CheilanthesTest.java
+++ dao/src/test/java/life/catalogue/es/nu/suggest/CheilanthesTest.java
@@ -7,6 +7,9 @@ import life.catalogue.api.search.NameUsageSuggestRequest;
 import life.catalogue.api.search.NameUsageSuggestResponse;
 import life.catalogue.es.EsReadTestBase;
 import life.catalogue.es.EsTestUtils;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class CheilanthesTest extends EsReadTestBase {
 
@@ -20,9 +23,21 @@ public class CheilanthesTest extends EsReadTestBase {
     EsTestUtils.indexCheilanthes(this);
     NameUsageSuggestRequest query = new NameUsageSuggestRequest();
     query.setDatasetKey(3);
-    query.setQ("Cheilanthes");
+    query.setQ("Cheilant");
     query.setFuzzy(true);
     NameUsageSuggestResponse nur = suggest(query);
+    assertEquals(1, nur.getSuggestions().size());
+  }
+
+  @Test
+  public void test02() throws IOException {
+    EsTestUtils.indexCheilanthes(this);
+    NameUsageSuggestRequest query = new NameUsageSuggestRequest();
+    query.setDatasetKey(3);
+    query.setQ("Che");
+    query.setFuzzy(false);
+    NameUsageSuggestResponse nur = suggest(query);
+    assertEquals(1, nur.getSuggestions().size());
   }
 
 }
