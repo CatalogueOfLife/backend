31fa6d61e759f91a1159a1ef276923d9cebb84e4
Markus <mdoering@gbif.org>
2020-08-20 14:57:37 +0200

    Fix project source metrics NPE

--- webservice/src/main/java/life/catalogue/resources/DatasetResource.java
+++ webservice/src/main/java/life/catalogue/resources/DatasetResource.java
@@ -232,13 +232,15 @@ public class DatasetResource extends AbstractGlobalResource<Dataset> {
     if (DatasetInfoCache.CACHE.origin(datasetKey) == DatasetOrigin.RELEASED) {
       Integer projectKey = DatasetInfoCache.CACHE.sourceProject(datasetKey);
       session.getMapper(SectorMapper.class).processDataset(datasetKey).forEach(s -> {
-        ImportMetrics m = sim.get(DSID.of(projectKey, key), s.getSyncAttempt());
-        metrics.add(m);
-        sectorCounter.incrementAndGet();
+        if (s.getSyncAttempt() != null) {
+          SectorImport m = sim.get(DSID.of(projectKey, key), s.getSyncAttempt());
+          metrics.add(m);
+          sectorCounter.incrementAndGet();
+        }
       });
 
     } else {
-      for (ImportMetrics m : sim.list(null, datasetKey, key, null, true, null)) {
+      for (SectorImport m : sim.list(null, datasetKey, key, null, true, null)) {
         metrics.add(m);
         sectorCounter.incrementAndGet();
       }
