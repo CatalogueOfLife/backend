c7c5f87f9a1ba3978494b6ba6f750a33f8695807
Markus <mdoering@gbif.org>
2020-09-24 20:20:21 +0200

    Test lowerCaseSerde for NameUsageSearchParameter

---
+++ api/src/test/java/life/catalogue/api/search/NameUsageSearchParameterSerdeTest.java
@@ -0,0 +1,26 @@
+package life.catalogue.api.search;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import life.catalogue.api.jackson.ApiModule;
+import life.catalogue.api.jackson.SerdeMapEnumKeyTestBase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NameUsageSearchParameterSerdeTest extends SerdeMapEnumKeyTestBase<NameUsageSearchParameter> {
+
+  public NameUsageSearchParameterSerdeTest() {
+    super(NameUsageSearchParameter.class);
+  }
+
+  @Test
+  public void noWhitespace() throws JsonProcessingException {
+    for (NameUsageSearchParameter p : NameUsageSearchParameter.values()) {
+      String json = ApiModule.MAPPER.writeValueAsString(p);
+      System.out.println(p +" -> " + json);
+      assertFalse(json.contains(" "));
+      assertTrue(Character.isLowerCase(json.charAt(1)));
+    }
+  }
+}
