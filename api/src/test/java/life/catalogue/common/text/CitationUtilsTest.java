b42f8d1ef449892dbc23fac9de95a7896cbd72a6
Markus <mdoering@gbif.org>
2020-10-09 01:44:55 +0200

    Fix build citation expectations

--- api/src/test/java/life/catalogue/common/text/CitationUtilsTest.java
+++ api/src/test/java/life/catalogue/common/text/CitationUtilsTest.java
@@ -50,18 +50,18 @@ public class CitationUtilsTest {
     d.setTitle("Species 2000 & ITIS Catalogue of Life");
     d.setEditors(Person.parse(Lists.newArrayList("Roskov Y.", "Ower G.", "Orrell T.", "Nicolson D.")));
     d.setReleased(LocalDate.parse("2019-04-21"));
-    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (eds.) (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21. Digital resource at www.catalogueoflife.org/col. Species 2000: Naturalis, Leiden, the Netherlands. ISSN 2405-8858.",
+    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (eds.) (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21.",
       CitationUtils.buildCitation(d)
     );
 
     d.setAuthors(d.getEditors());
     d.setEditors(Collections.emptyList());
-    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21. Digital resource at www.catalogueoflife.org/col. Species 2000: Naturalis, Leiden, the Netherlands. ISSN 2405-8858.",
+    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21.",
       CitationUtils.buildCitation(d)
     );
 
     d.setEditors(null);
-    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21. Digital resource at www.catalogueoflife.org/col. Species 2000: Naturalis, Leiden, the Netherlands. ISSN 2405-8858.",
+    assertEquals("Roskov Y., Ower G., Orrell T., Nicolson D. (2019). Species 2000 & ITIS Catalogue of Life, 2019-04-21.",
       CitationUtils.buildCitation(d)
     );
   }
