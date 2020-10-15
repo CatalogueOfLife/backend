f3b99a7e60b8272ea8d65a8e29becf23e23100a0
Markus <mdoering@gbif.org>
2020-10-15 17:55:40 +0200

    Add sectorKey and sectorDatasetKey search params to vernacular and ref search

--- api/src/main/java/life/catalogue/api/search/VernacularSearchRequest.java
+++ api/src/main/java/life/catalogue/api/search/VernacularSearchRequest.java
@@ -11,6 +11,21 @@ public class VernacularSearchRequest {
   @QueryParam("language")
   private String language;
 
+  /**
+   * The sector key attached to a taxon. Synonyms inherit the key by their accepted taxon, but do not expose the key on the Synonym instance
+   * itself.
+   */
+  @QueryParam("sectorKey")
+  private Integer sectorKey;
+
+  /**
+   * The subject dataset key of the corresponding sector attached to a taxon. Synonyms inherit the key by their accepted taxon, but do not expose
+   * the key on the Synonym instance itself.
+   */
+  @QueryParam("sectorDatasetKey")
+  private Integer sectorDatasetKey;
+
+
   public static VernacularSearchRequest byQuery(String query) {
     VernacularSearchRequest q = new VernacularSearchRequest();
     q.q = query;
@@ -33,17 +48,35 @@ public class VernacularSearchRequest {
     this.language = language;
   }
 
+  public Integer getSectorKey() {
+    return sectorKey;
+  }
+
+  public void setSectorKey(Integer sectorKey) {
+    this.sectorKey = sectorKey;
+  }
+
+  public Integer getSectorDatasetKey() {
+    return sectorDatasetKey;
+  }
+
+  public void setSectorDatasetKey(Integer sectorDatasetKey) {
+    this.sectorDatasetKey = sectorDatasetKey;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
     if (!(o instanceof VernacularSearchRequest)) return false;
     VernacularSearchRequest that = (VernacularSearchRequest) o;
     return Objects.equals(q, that.q) &&
-      Objects.equals(language, that.language);
+      Objects.equals(language, that.language) &&
+      Objects.equals(sectorKey, that.sectorKey) &&
+      Objects.equals(sectorDatasetKey, that.sectorDatasetKey);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(q, language);
+    return Objects.hash(q, language, sectorKey, sectorDatasetKey);
   }
 }
