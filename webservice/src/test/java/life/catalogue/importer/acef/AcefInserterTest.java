e5f9a4de19358e0c3d59d93d59e6b9f6369a306a
Markus <mdoering@gbif.org>
2020-10-21 14:53:17 +0200

    Final org test fixes

--- webservice/src/test/java/life/catalogue/importer/acef/AcefInserterTest.java
+++ webservice/src/test/java/life/catalogue/importer/acef/AcefInserterTest.java
@@ -1,8 +1,8 @@
 package life.catalogue.importer.acef;
 
-import com.google.common.collect.Lists;
 import life.catalogue.api.model.DatasetSettings;
 import life.catalogue.api.model.DatasetWithSettings;
+import life.catalogue.api.model.Organisation;
 import life.catalogue.api.model.Person;
 import life.catalogue.api.vocab.DataFormat;
 import life.catalogue.api.vocab.DatasetType;
@@ -13,6 +13,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -38,10 +39,10 @@ public class AcefInserterTest extends InserterBaseTest {
     //assertEquals("2014-05-05", d.getReleased().toString());
     assertEquals(1, d.getAuthors().size());
     assertEquals(Person.parse("YR Roskov & JL"), d.getContact());
-    assertThat(d.getAuthors(), is(Lists.newArrayList(Person.parse("Roskov Y.R."))));
+    assertThat(d.getAuthors(), is(List.of(Person.parse("Roskov Y.R."))));
     assertEquals("Legumes", d.getGroup());
     assertEquals("The International Legume Database & Information Service (ILDIS) is an international project which aims to document and catalogue the world's legume species diversity in a readily accessible form. Research groups in many countries are participating on a co-operative basis to pool information in the ILDIS World Database of Legumes, which is used to provide a worldwide information service through publications, electronic access and enquiry services.", d.getDescription());
-    assertThat(d.getOrganisations(), is(Lists.newArrayList("International")));
+    assertThat(d.getOrganisations(), is(List.of(Organisation.parse("International"))));
     assertEquals("http://www.ildis.org", d.getWebsite().toString());
     assertEquals((Integer)96, d.getCompleteness());
     assertEquals((Integer)4, d.getConfidence());
