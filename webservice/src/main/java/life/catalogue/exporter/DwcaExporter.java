59313fa1149f092696f75b8bcfaffa1c33df6c3f
Markus <mdoering@gbif.org>
2020-11-26 12:22:52 +0100

    Fix getTaxonInfo for non existing verbatim sources

--- webservice/src/main/java/life/catalogue/exporter/DwcaExporter.java
+++ webservice/src/main/java/life/catalogue/exporter/DwcaExporter.java
@@ -1,2 +1,45 @@
-package life.catalogue.exporter;public class DwcaExporter {
+package life.catalogue.exporter;
+
+import life.catalogue.common.concurrent.BackgroundJob;
+import life.catalogue.common.concurrent.JobPriority;
+
+import java.io.File;
+
+public class DwcaExporter extends BackgroundJob {
+
+  File archive;
+  final int datasetKey;
+
+  DwcaExporter(int datasetKey, int userKey, JobPriority priority) {
+    super(priority, userKey);
+    this.datasetKey = datasetKey;
+  }
+
+  public static DwcaExporter dataset(int datasetKey, int userKey) {
+    return new DwcaExporter(datasetKey, userKey, JobPriority.MEDIUM);
+  }
+
+  @Override
+  public void execute() throws Exception {
+    exportCore();
+    exportExtensions();
+    exportMetadata();
+    bundle();
+  }
+
+  private void bundle() {
+
+  }
+
+  private void exportMetadata() {
+
+  }
+
+  private void exportExtensions() {
+
+  }
+
+  private void exportCore() {
+  }
+
 }
