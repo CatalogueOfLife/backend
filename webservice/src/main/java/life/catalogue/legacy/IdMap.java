9abfcbc58188f1ec01cf53f3eb182ea3b3647348
Markus <mdoering@gbif.org>
2020-12-02 13:28:53 +0100

    Fix IdMap builder for missing source file

--- webservice/src/main/java/life/catalogue/legacy/IdMap.java
+++ webservice/src/main/java/life/catalogue/legacy/IdMap.java
@@ -57,6 +57,8 @@ public class IdMap implements Managed {
   }
 
   public static IdMap fromURI(File file, URI tsv) throws IOException {
+    if (tsv == null) return empty(file);
+    
     final URL url = tsv.toURL();
     return new IdMap(file, tsv.toString(), url::openStream);
   }
