e3a9e17e2d859928483efffd2ee37da36d122673
Markus Döring <mdoering@gbif.org>
2021-03-14 08:35:05 +0100

    Fix eml writer

--- webservice/src/test/java/life/catalogue/exporter/DwcaExporterTest.java
+++ webservice/src/test/java/life/catalogue/exporter/DwcaExporterTest.java
@@ -5,11 +5,15 @@ import life.catalogue.api.vocab.Users;
 import life.catalogue.db.PgSetupRule;
 import life.catalogue.db.TestDataRule;
 import life.catalogue.db.mapper.DatasetMapper;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class DwcaExporterTest extends ExporterTest {
@@ -24,12 +28,19 @@ public class DwcaExporterTest extends ExporterTest {
 
   @Test
   public void eml() throws Exception {
+    File f = File.createTempFile("col-eml", ".xml");
     try (SqlSession session = PgSetupRule.getSqlSessionFactory().openSession()) {
       DatasetMapper dm = session.getMapper(DatasetMapper.class);
 
-      File f = File.createTempFile("col-eml", ".xml");
       Dataset d = dm.get(TestDataRule.APPLE.key);
       DwcaExporter.writeEml(d, f);
+
+      String eml = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+      System.out.println(eml);
+      assertFalse(eml.contains("COL backend services"));
+
+    } finally {
+      f.delete();
     }
   }
 }
