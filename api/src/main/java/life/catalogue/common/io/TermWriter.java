c6f8b5bae80a6c7c4fa14586368285ff3a1cfe26
Markus Döring <m.doering@mac.com>
2021-11-15 22:18:29 +0100

    Fix rare exporter issue with non existing export directory

--- api/src/main/java/life/catalogue/common/io/TermWriter.java
+++ api/src/main/java/life/catalogue/common/io/TermWriter.java
@@ -44,6 +44,9 @@ public abstract class TermWriter implements AutoCloseable {
 
     static RowWriter setupWriter(File dir, Term rowType) {
       File f = new File(dir, filename(rowType));
+      if (!dir.exists()) {
+        dir.mkdirs();
+      }
       return TabWriter.fromFile(f);
     }
   }
