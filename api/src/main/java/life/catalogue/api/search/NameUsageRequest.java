ca3588048f3e2f75700c5d9cdcdb0d9428441ae9
Markus Döring <m.doering@mac.com>
2023-01-20 19:54:33 +0100

    Use enum names also for ES requests

--- api/src/main/java/life/catalogue/api/search/NameUsageRequest.java
+++ api/src/main/java/life/catalogue/api/search/NameUsageRequest.java
@@ -133,7 +133,7 @@ public abstract class NameUsageRequest {
 
   public void addFilter(NameUsageSearchParameter param, Enum<?> value) {
     nonNull(value);
-    addFilter(param, String.valueOf(value.ordinal()));
+    addFilter(param, value.name());
   }
 
   public void addFilter(NameUsageSearchParameter param, UUID value) {
@@ -200,7 +200,7 @@ public abstract class NameUsageRequest {
    * Extracts all query parameters that match a NameSearchParameter and registers them as query
    * filters. Values of query parameters that are associated with an enum type can be supplied using
    * the name of the enum constant or using the ordinal of the enum constant. In both cases it is the
-   * ordinal that will be registered as the query filter.
+   * name that will be registered as the query filter.
    */
   public void addFilters(MultivaluedMap<String, String> params) {
     params.entrySet().stream().filter(e -> !NON_FILTERS.contains(e.getKey())).forEach(e -> {
@@ -259,17 +259,8 @@ public abstract class NameUsageRequest {
         throw illegalValueForParameter(param, value);
       }
     } else if (param.type().isEnum()) {
-      try {
-        int i = Integer.parseInt(value);
-        if (i < 0 || i >= param.type().getEnumConstants().length) {
-          throw illegalValueForParameter(param, value);
-        }
-        addFilterValue(param, Integer.valueOf(i));
-      } catch (NumberFormatException e) {
-        @SuppressWarnings("unchecked")
         Enum<?> c = VocabularyUtils.lookupEnum(value, (Class<? extends Enum<?>>) param.type());
-        addFilterValue(param, Integer.valueOf(c.ordinal()));
-      }
+        addFilterValue(param, c.name());
     } else {
       throw new IllegalArgumentException("Unexpected parameter type: " + param.type());
     }
