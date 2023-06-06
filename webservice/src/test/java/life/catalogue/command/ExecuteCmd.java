0ef2731c920f62d58fbc3fc52d6237b14aed0023
Markus Döring <m.doering@mac.com>
2023-06-06 14:11:59 +0200

    Revert "Merge master"
    This reverts commit 306a31784e76348f6c4aa45207ea44a86513ddd4.

--- webservice/src/test/java/life/catalogue/command/ExecuteCmd.java
+++ webservice/src/test/java/life/catalogue/command/ExecuteCmd.java
@@ -15,8 +15,7 @@ public class ExecuteCmd {
   @Test
   public void test() throws Exception {
     // to run a command that needs configs please point the second argument to a matching yaml file
-    //new WsServer().run(new String[]{"index", "/Users/markus/code/col/backend/webservice/config-dev.yaml", "--prompt", "0", "--key", "1049"});
-    new WsServer().run(new String[]{"init", "/Users/markus/code/col/backend/webservice/config-scrap.yaml", "--prompt", "0", "--num", "16"});
-    //new WsServer().run(new String[]{"migrate", "/Users/markus/code/col/backend/webservice/config-local.yaml", "--prompt", "0", "--num", "6"});
+    //new WsServer().run(new String[]{"init", "/Users/markus/code/col/backend/webservice/config-local.yaml", "--prompt", "0", "--num", "4"});
+    new WsServer().run(new String[]{"index", "/Users/markus/code/col/backend/webservice/config-dev.yaml", "--prompt", "0", "--key", "1049"});
   }
 }
