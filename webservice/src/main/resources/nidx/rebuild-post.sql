d8e62ed0f151f359be61b8c75862ebc414beb3ee
Markus Döring <m.doering@mac.com>
2023-07-01 10:38:37 +0200

    Add all names index indices when rebuilding

--- webservice/src/main/resources/nidx/rebuild-post.sql
+++ webservice/src/main/resources/nidx/rebuild-post.sql
@@ -1,4 +1,7 @@
 CREATE INDEX ON nidx.names_index (canonical_id);
+CREATE INDEX ON nidx.names_index (scientific_name);
+CREATE INDEX ON nidx.names_index (scientific_name) WHERE id = canonical_id;
+
 CREATE INDEX ON nidx.name_match (dataset_key, sector_key);
 CREATE INDEX ON nidx.name_match (dataset_key, index_id);
 CREATE INDEX ON nidx.name_match (index_id);
@@ -8,3 +11,5 @@ ALTER TABLE nidx.names_index ADD FOREIGN KEY (canonical_id) REFERENCES nidx.name
 
 ALTER TABLE nidx.name_match ADD PRIMARY KEY (dataset_key, name_id);
 ALTER TABLE nidx.name_match ADD FOREIGN KEY (index_id) REFERENCES nidx.names_index;
+ALTER TABLE nidx.name_match ADD FOREIGN KEY (dataset_key, sector_key) REFERENCES sector;
+ALTER TABLE nidx.name_match ADD FOREIGN KEY (dataset_key, name_id) REFERENCES name;
