af03e93fb1b64b393ea18b46eef9c4bb857d6aa6
Markus Döring <m.doering@mac.com>
2023-08-18 13:08:07 +0200

    Add specific exception mapper for synonyms

--- webservice/src/main/java/life/catalogue/dw/jersey/exception/SynonymExceptionMapper.java
+++ webservice/src/main/java/life/catalogue/dw/jersey/exception/SynonymExceptionMapper.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import io.dropwizard.jersey.errors.ErrorMessage;
 
-import life.catalogue.api.exception.ArchivedException;
 import life.catalogue.api.exception.NotFoundException;
-
-import life.catalogue.api.model.ArchivedNameUsage;
+import life.catalogue.api.exception.SynonymException;
+import life.catalogue.api.model.DSID;
 import life.catalogue.api.model.NameUsage;
 
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,38 +19,38 @@ import javax.ws.rs.ext.Provider;
 import static org.apache.commons.lang3.StringUtils.trimToNull;
 
 /**
- * Converts a {@link ArchivedException} into a 404.
+ * Converts a {@link SynonymException} into a 404.
  */
 @Provider
-public class ArchivedExceptionMapper extends JsonExceptionMapperBase<ArchivedException> {
-  private static final Logger LOG = LoggerFactory.getLogger(ArchivedExceptionMapper.class);
+public class SynonymExceptionMapper extends JsonExceptionMapperBase<SynonymException> {
+  private static final Logger LOG = LoggerFactory.getLogger(SynonymExceptionMapper.class);
   private static final Response.Status STATUS = Response.Status.NOT_FOUND;
 
-  public ArchivedExceptionMapper() {
+  public SynonymExceptionMapper() {
     super(STATUS, true, false, null);
   }
 
   @Override
-  public Response toResponse(ArchivedException ex) {
+  public Response toResponse(SynonymException ex) {
     LOG.debug("{}: {}", ex.getClass().getSimpleName(), ex.getMessage());
     return Response
       .status(STATUS)
       .type(MediaType.APPLICATION_JSON_TYPE)
-      .entity(new ArchivedMessage(ex))
+      .entity(new SynonymExceptionMessage(ex))
       .build();
   }
 
   @JsonInclude(JsonInclude.Include.NON_NULL)
-  public class ArchivedMessage extends ErrorMessage {
-    private final ArchivedNameUsage usage;
+  public class SynonymExceptionMessage extends ErrorMessage {
+    private final DSID<String> acceptedKey;
 
-    public ArchivedMessage(ArchivedException e) {
+    public SynonymExceptionMessage(SynonymException e) {
       super(STATUS.getStatusCode(), trimToNull(e.getMessage()));
-      this.usage = e.usage;
+      this.acceptedKey = e.acceptedKey;
     }
 
-    public NameUsage getUsage() {
-      return usage;
+    public String getAcceptedId() {
+      return acceptedKey.getId();
     }
   }
 }
