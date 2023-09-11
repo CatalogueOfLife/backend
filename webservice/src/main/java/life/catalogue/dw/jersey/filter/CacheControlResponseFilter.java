0397c969e2c68b27db3a7bfcc19ef5e62c46e571
Markus Döring <m.doering@mac.com>
2023-09-11 13:02:41 +0200

    Allow only 1h of caching for vocabs,openapi&version resource

--- webservice/src/main/java/life/catalogue/dw/jersey/filter/CacheControlResponseFilter.java
+++ webservice/src/main/java/life/catalogue/dw/jersey/filter/CacheControlResponseFilter.java
@@ -26,9 +26,8 @@ import it.unimi.dsi.fastutil.ints.IntSet;
  */
 public class CacheControlResponseFilter implements ContainerResponseFilter {
   private static final Logger LOG = LoggerFactory.getLogger(CacheControlResponseFilter.class);
-  private static final long MAX_AGE = TimeUnit.HOURS.toSeconds(24);
-  // age in seconds
-  private static final String CACHE24 = "public, max-age=" + MAX_AGE + ", s-maxage=" + MAX_AGE;
+  private static final long AGE1 = TimeUnit.HOURS.toSeconds(1);
+  private static final long AGE24 = TimeUnit.HOURS.toSeconds(24);
   private static final Pattern DATASET_PATH  = Pattern.compile("dataset/(\\d+)");
   private static final Pattern STATIC_PATH  = Pattern.compile("^(vocab|openapi|version)");
   private static final Set<String> METHODS  = Set.of(HttpMethod.GET, HttpMethod.HEAD);
@@ -38,7 +37,7 @@ public class CacheControlResponseFilter implements ContainerResponseFilter {
   public void filter(ContainerRequestContext req, ContainerResponseContext resp) throws IOException {
     if (req.getMethod() != null && METHODS.contains(req.getMethod())) {
       if (STATIC_PATH.matcher(req.getUriInfo().getPath()).find()) {
-        allowCaching(resp);
+        allowCaching(resp, AGE1);
         return;
       }
       Matcher m = DATASET_PATH.matcher(req.getUriInfo().getPath());
@@ -47,7 +46,7 @@ public class CacheControlResponseFilter implements ContainerResponseFilter {
         int datasetKey = Integer.parseInt(m.group(1));
         if (releases.contains(datasetKey)) {
           // its a release, we can cache it!
-          allowCaching(resp);
+          allowCaching(resp, AGE24);
           return;
         }
       }
@@ -65,8 +64,8 @@ public class CacheControlResponseFilter implements ContainerResponseFilter {
     return releases.addAll(keys);
   }
 
-  private void allowCaching(ContainerResponseContext resp){
-    resp.getHeaders().putSingle(HttpHeaders.CACHE_CONTROL, CACHE24);
+  private void allowCaching(ContainerResponseContext resp, long ageInSeconds){
+    resp.getHeaders().putSingle(HttpHeaders.CACHE_CONTROL, "public, max-age=" + ageInSeconds + ", s-maxage=" + ageInSeconds);
   }
 
   private void preventCaching(ContainerResponseContext resp){
