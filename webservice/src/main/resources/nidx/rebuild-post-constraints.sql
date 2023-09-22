a98649290b85a880eef4d20a4eb57282d08fc2dd
Markus Döring <m.doering@mac.com>
2023-09-22 11:05:57 +0200

    Only keep constraints to point to public tables in second post file

--- webservice/src/main/resources/nidx/rebuild-post-constraints.sql
+++ webservice/src/main/resources/nidx/rebuild-post-constraints.sql
@@ -1,4 +1,2 @@
-ALTER TABLE nidx.name_match ADD PRIMARY KEY (dataset_key, name_id);
-ALTER TABLE nidx.name_match ADD FOREIGN KEY (index_id) REFERENCES nidx.names_index;
 ALTER TABLE nidx.name_match ADD FOREIGN KEY (dataset_key, sector_key) REFERENCES sector;
 ALTER TABLE nidx.name_match ADD FOREIGN KEY (dataset_key, name_id) REFERENCES name;
--- webservice/src/main/resources/nidx/rebuild-post.sql
+++ webservice/src/main/resources/nidx/rebuild-post.sql
@@ -8,3 +8,6 @@ CREATE INDEX ON nidx.name_match (index_id);
 
 ALTER TABLE nidx.names_index ADD PRIMARY KEY (id);
 ALTER TABLE nidx.names_index ADD FOREIGN KEY (canonical_id) REFERENCES nidx.names_index;
+
+ALTER TABLE nidx.name_match ADD PRIMARY KEY (dataset_key, name_id);
+ALTER TABLE nidx.name_match ADD FOREIGN KEY (index_id) REFERENCES nidx.names_index;
