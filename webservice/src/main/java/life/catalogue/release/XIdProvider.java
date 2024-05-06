f37b270af8acc8dd4302f61216a703b5d41e885e
Markus Döring <mdoering@gbif.org>
2024-05-06 16:08:14 +0200

    Adds new stable is usage id generator

---
+++ webservice/src/main/java/life/catalogue/release/XIdProvider.java
@@ -0,0 +1,48 @@
+package life.catalogue.release;
+
+import life.catalogue.api.model.SimpleNameWithNidx;
+import life.catalogue.config.ReleaseConfig;
+
+import life.catalogue.matching.NameIndex;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+
+/**
+ * TODO: Share logic with IdProvider
+ */
+public class XIdProvider extends IdProvider implements UsageIdGen, AutoCloseable {
+  private final NameIndex nidx;
+  private final Writer nomatchWriter;
+
+  public XIdProvider(int projectKey, int attempt, int releaseDatasetKey, ReleaseConfig cfg, NameIndex nidx, SqlSessionFactory factory) throws IOException {
+    super(projectKey, attempt, releaseDatasetKey, cfg, factory);
+    this.nidx = nidx;
+    nomatchWriter = buildNomatchWriter();
+  }
+
+  @Override
+  public String issue(SimpleNameWithNidx usage) {
+    try {
+      issueIDs(usage.getNamesIndexId(), List.of(usage), nomatchWriter, false);
+      return encode(usage.getCanonicalId());
+
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public Integer nidx2canonical(Integer nidx) {
+    return this.nidx.getCanonical(nidx);
+  }
+
+  @Override
+  public void close() throws IOException {
+    nomatchWriter.close();
+  }
+}
