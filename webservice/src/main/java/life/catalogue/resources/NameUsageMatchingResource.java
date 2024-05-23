79a3e2de8857397d4cae8ceeaa72f97295bc9c45
Markus Döring <mdoering@gbif.org>
2024-05-23 16:55:14 +0200

    Use string query parameters in name matching to allow bad values, but flag them

--- webservice/src/main/java/life/catalogue/resources/NameUsageMatchingResource.java
+++ webservice/src/main/java/life/catalogue/resources/NameUsageMatchingResource.java
@@ -11,6 +11,8 @@ import life.catalogue.concurrent.JobExecutor;
 import life.catalogue.importer.NameInterpreter;
 import life.catalogue.matching.*;
 
+import life.catalogue.parser.*;
+
 import org.gbif.nameparser.api.NomCode;
 import org.gbif.nameparser.api.Rank;
 
@@ -53,14 +55,6 @@ public class NameUsageMatchingResource {
     this.matcher = matcher;
   }
 
-  private UsageMatchWithOriginal match(int datasetKey, SimpleNameClassified<SimpleName> sn, boolean verbose) {
-    if (StringUtils.isBlank(sn.getName())) {
-      throw new IllegalArgumentException("Missing name");
-    }
-    IssueContainer issues = new IssueContainer.Simple();
-    return match(datasetKey, sn, issues, verbose);
-  }
-
   private UsageMatchWithOriginal match(int datasetKey, SimpleNameClassified<SimpleName> sn, IssueContainer issues, boolean verbose) {
     UsageMatch match;
     var opt = interpreter.interpret(sn, issues);
@@ -79,6 +73,35 @@ public class NameUsageMatchingResource {
   }
 
 
+  private static SimpleNameClassified<SimpleName> interpret(String id,
+                                                     String q,
+                                                     String name,
+                                                     String sciname,
+                                                     String authorship,
+                                                     String code,
+                                                     String rank,
+                                                     String status,
+                                                     Classification classification,
+                                                     IssueContainer issues) {
+    NomCode iCode = SafeParser.parse(NomCodeParser.PARSER, code)
+      .orNull(Issue.NOMENCLATURAL_CODE_INVALID, issues);
+
+    Rank iRank = SafeParser.parse(RankParser.PARSER, rank)
+      .orNull(Issue.RANK_INVALID, issues);
+
+    EnumNote<TaxonomicStatus> iStatus = SafeParser.parse(TaxonomicStatusParser.PARSER, status)
+      .orElse(()->new EnumNote<>(TaxonomicStatus.ACCEPTED, null), Issue.TAXONOMIC_STATUS_INVALID, issues);
+
+    var sn = SimpleNameClassified.snc(id, iRank, iCode, iStatus.val, ObjectUtils.coalesce(sciname, name, q), authorship);
+    if (StringUtils.isBlank(sn.getName())) {
+      throw new IllegalArgumentException("Missing name");
+    }
+    if (classification != null) {
+      sn.setClassification(classification.asSimpleNames());
+    }
+    return sn;
+  }
+
   @GET
   public UsageMatchWithOriginal match(@PathParam("key") int datasetKey,
                                       @QueryParam("id") String id,
@@ -86,20 +109,15 @@ public class NameUsageMatchingResource {
                                       @QueryParam("name") String name,
                                       @QueryParam("scientificName") String sciname,
                                       @QueryParam("authorship") String authorship,
-                                      @QueryParam("code") NomCode code,
-                                      @QueryParam("rank") Rank rank,
-                                      @QueryParam("status") @DefaultValue("ACCEPTED") TaxonomicStatus status,
+                                      @QueryParam("code") String code,
+                                      @QueryParam("rank") String rank,
+                                      @QueryParam("status") String status,
                                       @QueryParam("verbose") boolean verbose,
                                       @BeanParam Classification classification
   ) throws InterruptedException {
-    if (status == TaxonomicStatus.BARE_NAME) {
-      throw new IllegalArgumentException("Cannot match a bare name to a name usage");
-    }
-    SimpleNameClassified<SimpleName> orig = SimpleNameClassified.snc(id, rank, code, status, ObjectUtils.coalesce(sciname, name, q), authorship);
-    if (classification != null) {
-      orig.setClassification(classification.asSimpleNames());
-    }
-    return match(datasetKey, orig, verbose);
+    IssueContainer issues = new IssueContainer.Simple();
+    SimpleNameClassified<SimpleName> orig = interpret(id, q, name, sciname, authorship, code, rank, status, classification, issues);
+    return match(datasetKey, orig, issues, verbose);
   }
 
   private MatchingJob submit(MatchingRequest req, User user) {
