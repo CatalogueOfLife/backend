bf30b4679657baa169a64c9174c87c926a008664
Dave Martin <djtfmartin@gmail.com>
2024-06-12 18:14:14 +0100

    simple services for ID lookup #2 https://github.com/CatalogueOfLife/backend/issues/1318

--- matching-ws/src/main/java/life/catalogue/matching/IDController.java
+++ matching-ws/src/main/java/life/catalogue/matching/IDController.java
@@ -1,5 +1,8 @@
 package life.catalogue.matching;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.extensions.ExtensionProperty;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,29 +10,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class IDController {
 
   @Autowired
   MatchingService matchingService;
 
+  @Operation(
+    operationId = "idLookupByDataset",
+    summary = "ID lookup service by dataset",
+    description =
+      "A lookup service, largely intended for debug purposes, to enable" +
+        "verification that an external ID has been indexed and to check if" +
+        "the ID has been associated with a taxon in the main index. ")
   @ApiResponse(responseCode = "200", description = "Name usage suggestions found")
   @Tag(name = "ID lookup")
   @GetMapping(
     value = {"v2/id/{datasetId}/{identifier}"},
     produces = "application/json")
-  public Object matchV2(
+  public List<ExternalID> matchV2(
     @PathVariable(value = "datasetId", required = false) String datasetId,
     @PathVariable(value = "identifier", required = false) String identifier) {
     return matchingService.matchID(datasetId, identifier);
   }
 
+  @Operation(
+    operationId = "idLookup",
+    summary = "ID lookup service",
+    description =
+      "A lookup service, largely intended for debug purposes, to enable" +
+        "verification that an external ID has been indexed and to check if" +
+        "the ID has been associated with a taxon in the main index. ")
   @ApiResponse(responseCode = "200", description = "Name usage suggestions found")
   @Tag(name = "ID lookup")
   @GetMapping(
     value = {"v2/id/{identifier}"},
     produces = "application/json")
-  public Object matchV2(
+  public List<ExternalID> matchV2(
     @PathVariable(value = "identifier", required = false) String identifier) {
     return matchingService.matchID(identifier);
   }
