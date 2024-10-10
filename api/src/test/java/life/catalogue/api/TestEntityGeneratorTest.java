636f72f6d7eef935594b457e28024b177d8b0996
Markus Döring <mdoering@gbif.org>
2024-10-10 15:08:33 +0200

    Fix commons lang3 version

--- api/src/test/java/life/catalogue/api/TestEntityGeneratorTest.java
+++ api/src/test/java/life/catalogue/api/TestEntityGeneratorTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertNotNull;
 
 public class TestEntityGeneratorTest {
 
+  /**
+   * Jenkins builds got stuck with commons lang3 version 3.17.0 creating a verbatim test record.
+   */
   @Test
   public void randomUnicodeString() {
     var v = TestEntityGenerator.createVerbatim();
