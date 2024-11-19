e6640b021057ac62718450fb5c0f8dd79a307eb2
Markus Döring <mdoering@gbif.org>
2024-11-19 10:56:42 +0100

    Interpret more kingdom specific name endings

--- parser/src/test/java/life/catalogue/parser/TaxGroupParserTest.java
+++ parser/src/test/java/life/catalogue/parser/TaxGroupParserTest.java
@@ -35,8 +35,15 @@ public class TaxGroupParserTest extends ParserTestBase<TaxGroup> {
     // recognize suffix
     assertParse(TaxGroup.Algae, "Blablaphyceae");
     assertParse(TaxGroup.Fungi, "Blablamycetes");
+    assertParse(TaxGroup.Plants, "Blablaphytina");
+    assertParse(TaxGroup.Viruses, "Blablaviridae");
     // but not for binomials
     assertParse(null, "Blabla blamycetes");
+    assertParse(null, "Blabla blaviridae");
+    assertParse(null, "Blabla blaphytina");
+    // zoological genus
+    assertParse(null, "Neophyta");
+    assertParse(null, "Protophyta");
   }
 
   @Test
