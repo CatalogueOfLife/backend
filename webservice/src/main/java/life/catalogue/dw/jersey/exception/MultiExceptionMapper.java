8acb6986284604f7cc59da5797cc7f7426df14d2
Markus Döring <mdoering@gbif.org>
2025-01-07 18:01:36 +0100

    Avoid base mapper for MultiExceptionMapper

--- webservice/src/main/java/life/catalogue/dw/jersey/exception/MultiExceptionMapper.java
+++ webservice/src/main/java/life/catalogue/dw/jersey/exception/MultiExceptionMapper.java
@@ -1,28 +1,21 @@
 package life.catalogue.dw.jersey.exception;
 
 import jakarta.ws.rs.BadRequestException;
-
-import life.catalogue.api.exception.NotFoundException;
-
-import life.catalogue.img.UnsupportedFormatException;
-
-import org.glassfish.hk2.api.MultiException;
-
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
-
+import life.catalogue.img.UnsupportedFormatException;
+import org.glassfish.hk2.api.MultiException;
 import org.glassfish.jersey.server.ParamException;
+
+import static life.catalogue.dw.jersey.exception.JsonExceptionMapperBase.jsonErrorResponse;
 
 /**
  * Converts a {@link MultiException} into a 400 bad request if the underlying cause maps.
  * Otherwise falls back to a default 500 server error.
  */
 @Provider
-public class MultiExceptionMapper extends JsonExceptionMapperBase<MultiException> {
-
-  public MultiExceptionMapper() {
-    super(Response.Status.BAD_REQUEST);
-  }
+public class MultiExceptionMapper implements ExceptionMapper<MultiException> {
 
   @Override
   public Response toResponse(MultiException ex) {
