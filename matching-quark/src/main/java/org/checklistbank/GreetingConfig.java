e45c0c36d0f69e3f55da52ca9d85cac4a4a041de
Markus Döring <mdoering@gbif.org>
2025-01-31 12:46:44 +0100

    Add quarkus out of the box

---
+++ matching-quark/src/main/java/org/checklistbank/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.checklistbank;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+  @WithName("message")
+  String message();
+
+}
