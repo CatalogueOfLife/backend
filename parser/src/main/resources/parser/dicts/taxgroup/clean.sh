8f84cf50b30495b68d4b0f2f61756f4f87ee33e5
Markus Döring <mdoering@gbif.org>
2025-02-13 16:15:30 +0100

    Large addition to taxgroup dicts using COL All Living Families

--- parser/src/main/resources/parser/dicts/taxgroup/clean.sh
+++ parser/src/main/resources/parser/dicts/taxgroup/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for fn in *.txt; do
   mv "$fn" "$fn.orig"
-  cat "$fn.orig" | sort | uniq > "$fn"
+  cat "$fn.orig" | sort | uniq | sed -E '/^(.*N\.N\..*)?$/d' > "$fn"
 done
 rm *.orig
