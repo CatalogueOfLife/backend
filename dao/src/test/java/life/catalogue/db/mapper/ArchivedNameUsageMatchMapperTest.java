95706afcc31a7e5887693b9324c7f7d75a526e3d
Markus Döring <mdoering@gbif.org>
2025-04-10 18:33:41 +0200

    Remove CRUD tests from archived name mappers

--- dao/src/test/java/life/catalogue/db/mapper/ArchivedNameUsageMatchMapperTest.java
+++ dao/src/test/java/life/catalogue/db/mapper/ArchivedNameUsageMatchMapperTest.java
@@ -15,53 +15,25 @@ import static org.junit.Assert.assertEquals;
  */
 public class ArchivedNameUsageMatchMapperTest extends MapperTestBase<ArchivedNameUsageMatchMapper> {
 
-  private int datasetKey;
-  private ArchivedNameUsage u;
-  IndexName nidx;
-
   public ArchivedNameUsageMatchMapperTest() {
     super(ArchivedNameUsageMatchMapper.class);
   }
-  
-  @Before
-  public void initMappers() {
-    datasetKey = testDataRule.testData.key;
-    u = ArchivedNameUsageMapperTest.create();
-    mapper(ArchivedNameUsageMapper.class).create(u);
 
-    // add names index match
-    var nim = mapper(NamesIndexMapper.class);
-    nidx = new IndexName(u.getName());
-    nim.create(nidx);
-
-    var nmm = mapper(ArchivedNameUsageMatchMapper.class);
-    nmm.create(DSID.of(3, u.getId()), nidx.getKey(), MatchType.EXACT);
-    commit();
-  }
 
   @Test
   public void get() throws Exception {
-    mapper().get(DSID.of(datasetKey, u.getId()));
+    mapper().get(DSID.of(appleKey, "xxx"));
   }
 
   @Test
   public void deleteOrphaned() throws Exception {
     // no real data to delete but tests valid SQL
-    mapper().deleteOrphans(datasetKey);
+    mapper().deleteOrphans(appleKey);
   }
 
   @Test
   public void processIndexIds() throws Exception {
-    mapper().processIndexIds(datasetKey).forEach(System.out::println);
-  }
-
-  @Test
-  public void updateMatches() throws Exception {
-    Integer nidx = 1;
-    mapper().update(u, nidx, MatchType.EXACT);
-    var n = mapper().get(u);
-    assertEquals(MatchType.EXACT, n.getType());
-    assertEquals(nidx, n.getName().getKey());
+    mapper().processIndexIds(appleKey).forEach(System.out::println);
   }
   
 }
