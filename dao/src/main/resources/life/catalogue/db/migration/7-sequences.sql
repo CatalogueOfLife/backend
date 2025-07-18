a7655a284d4999d7a1a38b7eb7941c468c38cfc0
Markus Döring <mdoering@gbif.org>
2025-07-18 09:48:50 +0200

    Remove unused migration sql and fix sector import mappers use of verbatim_source

--- dao/src/main/resources/life/catalogue/db/migration/7-sequences.sql
+++
@@ -1,5 +0,0 @@
-
--- GLOBAL SEQUENCES
-SELECT setval('public.dataset_key_seq', (select max(key)+1 from dataset));
-SELECT setval('public.names_index_id_seq', (SELECT COALESCE(max(id),1) FROM names_index));
-SELECT setval('public.user_key_seq', (SELECT COALESCE(max(key),1) FROM "user"));
