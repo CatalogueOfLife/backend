b87896209668d8e3c30d56f199605724308986d7
Markus Döring <mdoering@gbif.org>
2025-08-25 16:11:25 +0200

    Also delete secondary verbatim sources when removing orphans

---
+++ dao/src/test/java/life/catalogue/dao/VerbatimDaoTest.java
@@ -0,0 +1,26 @@
+package life.catalogue.dao;
+
+import life.catalogue.api.vocab.Users;
+import life.catalogue.es.NameUsageIndexService;
+import life.catalogue.junit.MybatisTestUtils;
+import life.catalogue.junit.SqlSessionFactoryRule;
+import life.catalogue.matching.nidx.NameIndexFactory;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VerbatimDaoTest extends DaoTestBase {
+
+  VerbatimDao dao;
+
+  @Before
+  public void init(){
+    dao = new VerbatimDao(factory());
+  }
+
+  @Test
+  public void orphans() {
+    dao.deleteOrphans(testDataRule.testData.key, Users.TESTER);
+  }
+}
