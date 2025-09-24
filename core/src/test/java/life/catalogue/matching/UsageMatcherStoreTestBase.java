25b99485856fff7dc9e84af1c95fc70f7f0aa37f
Markus Döring <mdoering@gbif.org>
2025-09-24 16:49:06 +0200

    Cater for NotFoundException in storage tests

--- core/src/test/java/life/catalogue/matching/UsageMatcherStoreTestBase.java
+++ core/src/test/java/life/catalogue/matching/UsageMatcherStoreTestBase.java
@@ -1,5 +1,6 @@
 package life.catalogue.matching;
 
+import life.catalogue.api.exception.NotFoundException;
 import life.catalogue.api.model.SimpleNameCached;
 
 import life.catalogue.api.vocab.TaxonomicStatus;
@@ -10,29 +11,27 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public abstract class UsageMatcherStoreTestBase {
+
+  static SimpleNameCached add(SimpleNameCached snc, UsageMatcherStore store) {
+    store.add(snc);
+    return new SimpleNameCached(snc);
+  }
 
   @Test
   public void basics() {
     try (UsageMatcherStore store = createStore(1)) {
       assertEquals(0, store.size());
 
-      var sn0 = snc("0", null, "Ausaceae", null, Rank.FAMILY, 0, 10);
-      var sn1 = snc("1", "4", "Aus bus", "Smith", Rank.SPECIES, 1, 11);
-      var sn2 = snc("2", "4", "Aus cus", "Miller", Rank.SPECIES, 2, 12);
-      var sn3 = snc("3", "1", "Aus bus cus", "(Miller)", Rank.SUBSPECIES, 3, 13);
-      var sn4 = snc("4", "0", "Aus", "Green", Rank.GENUS, 4, 14);
-      var sn5 = sncSyn("5", "2", "Aus cus", "Jackson", Rank.SPECIES, 2, 15);
+      var sn0 = add(snc("0", null, "Ausaceae", null, Rank.FAMILY, 0, 10), store);
+      var sn1 = add(snc("1", "4", "Aus bus", "Smith", Rank.SPECIES, 1, 11), store);
+      var sn2 = add(snc("2", "4", "Aus cus", "Miller", Rank.SPECIES, 2, 12), store);
+      var sn3 = add(snc("3", "1", "Aus bus cus", "(Miller)", Rank.SUBSPECIES, 3, 13), store);
+      var sn4 = add(snc("4", "0", "Aus", "Green", Rank.GENUS, 4, 14), store);
+      var sn5 = add(sncSyn("5", "2", "Aus cus", "Jackson", Rank.SPECIES, 2, 15), store);
 
-      store.add(sn0);
-      store.add(sn1);
-      store.add(sn2);
-      store.add(sn3);
-      store.add(sn4);
-      store.add(sn5);
       assertEquals(6, store.size());
 
       assertEquals(sn2, store.get(sn2.getId()));
@@ -52,7 +51,16 @@ public abstract class UsageMatcherStoreTestBase {
       assertEquals(sn5.getParentId(), cl.get(0).getId());
 
       assertEquals(sn4, store.get(sn4.getId()));
-      assertNull(store.get("4"));
+      assertNotFound("4", store);
+    }
+  }
+
+  static void assertNotFound(String id, UsageMatcherStore store) {
+    try {
+      store.get(id);
+      fail("Expected NotFoundException for ID " + id);
+    } catch (NotFoundException e) {
+      // expected
     }
   }
 
