5d08f40837ac1b74bf0386179a88bd273b9441c1
Markus Döring <mdoering@gbif.org>
2025-10-28 14:19:33 +0100

    Remove redundant target rematching

--- core/src/main/java/life/catalogue/assembly/TreeMergeHandler.java
+++ core/src/main/java/life/catalogue/assembly/TreeMergeHandler.java
@@ -78,16 +78,6 @@ public class TreeMergeHandler extends TreeBaseHandler {
     if (target != null) {
       var cl = target.getParentKey() == null ? null : num.getClassificationSN(target.getParentKey());
       trgt = utils.toSimpleNameClassified(target, cl);
-      // match target to tmp project identifiers, which usually differ from the project
-      if (matcher.getDatasetKey() != target.getDatasetKey()) {
-        UsageMatch match = matcher.match(trgt);
-        if (match.isMatch()) {
-          trgt = match.usage;
-        } else {
-          LOG.warn("Sector target not found in tmp release project: {}", trgt);
-          trgt = null;
-        }
-      }
     }
     if (trgt == null && cfg != null && cfg.incertae != null) {
       trgt = utils.toSimpleNameClassified(cfg.incertae, null);
