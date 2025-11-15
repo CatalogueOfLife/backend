81048fb3b4ab49266df45d4292527a64446a1514
Markus Döring <mdoering@gbif.org>
2025-11-15 08:50:26 +0100

    Finish publisher mapper and add crid tests

--- dao/src/test/java/life/catalogue/db/mapper/PublisherMapperTest.java
+++ dao/src/test/java/life/catalogue/db/mapper/PublisherMapperTest.java
@@ -2,6 +2,7 @@ package life.catalogue.db.mapper;
 
 import life.catalogue.api.model.Page;
 
+import life.catalogue.api.model.Publisher;
 import life.catalogue.api.vocab.Publishers;
 
 import org.junit.Test;
@@ -10,7 +11,7 @@ import java.util.UUID;
 
 import static org.junit.Assert.*;
 
-public class PublisherMapperTest  extends MapperTestBase<PublisherMapper> {
+public class PublisherMapperTest  extends CRUDTestBase<UUID, Publisher, PublisherMapper> {
 
   public PublisherMapperTest() {
     super(PublisherMapper.class);
@@ -29,5 +30,28 @@ public class PublisherMapperTest  extends MapperTestBase<PublisherMapper> {
   @Test
   public void exists() throws Exception {
     assertFalse(mapper().exists(Publishers.PLAZI));
+  }
+
+  @Override
+  Publisher createTestEntity() {
+    var p = new Publisher();
+    p.setKey(UUID.randomUUID());
+    p.setTitle("Publisher title");
+    p.setDescription("Publisher description");
+    p.setCountry("Germany");
+    p.setProvince("Berlin");
+    p.setCity("Berlin");
+    p.setHomepage("http://www.plazi.org");
+    p.setLatitude(52.51703);
+    p.setLongitude(13.40495);
+    return p;
+  }
+
+  @Override
+  void updateTestObj(Publisher p) {
+    p.setTitle("Updated publisher title");
+    p.setDescription("Updated description");
+    p.setLatitude(52.51);
+    p.setLongitude(13.40);
   }
 }
