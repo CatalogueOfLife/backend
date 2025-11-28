3c9c8712dbe6c2f196e20462cf9f545883d498b6
Markus Döring <mdoering@gbif.org>
2025-11-28 11:05:26 +0100

    Adapt thumbor test to ful url encoding

--- dao/src/test/java/life/catalogue/img/ThumborServiceTest.java
+++ dao/src/test/java/life/catalogue/img/ThumborServiceTest.java
@@ -14,7 +14,7 @@ public class ThumborServiceTest {
     cfg.key = "44567fd-re32d3223d-23-32-e-23d-3-d-32dd3";
     var thumbor = new ThumborService(cfg);
     var url = URI.create("https://nh.kanagawa-museum.jp/kpmnh-collections/rest/media/L?cls=media_attin&pkey=753428&c3022");
-    assertEquals("https://nh.kanagawa-museum.jp/kpmnh-collections/rest/media/L%3Fcls=media_attin&pkey=753428&c3022", ThumborService.urlEscape(url));
+    assertEquals("https%3A%2F%2Fnh.kanagawa-museum.jp%2Fkpmnh-collections%2Frest%2Fmedia%2FL%3Fcls%3Dmedia_attin%26pkey%3D753428%26c3022", ThumborService.urlEscape(url));
     System.out.println(thumbor.thumbnail(url));
   }
 
