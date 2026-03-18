def63cc50924e6fad8ecf7e3f942a2871640ebbd
Markus Döring <mdoering@gbif.org>
2026-03-18 14:45:40 +0100

    Avoid NPE when setting the accepted name of a search synonym results during tests

--- api/src/main/java/life/catalogue/api/search/NameUsageSearchResult.java
+++ api/src/main/java/life/catalogue/api/search/NameUsageSearchResult.java
@@ -3,6 +3,7 @@ package life.catalogue.api.search;
 import life.catalogue.api.model.SimpleName;
 import life.catalogue.api.model.Synonym;
 import life.catalogue.api.model.Taxon;
+import life.catalogue.api.vocab.TaxonomicStatus;
 
 public class NameUsageSearchResult extends NameUsageWrapper {
   private Double score;
@@ -13,8 +14,14 @@ public class NameUsageSearchResult extends NameUsageWrapper {
   public NameUsageSearchResult(NameUsageWrapper nuw) {
     super(nuw);
     if (getUsage() instanceof Synonym syn) { // a synonym
-      var acc = new Taxon(getParent());
-      syn.setAccepted(acc);
+      var sn = getParent();
+      if (sn != null) {
+        // null should only happen in tests, still...
+        if (sn.getStatus() == null) {
+          sn.setStatus(TaxonomicStatus.ACCEPTED);
+        }
+        syn.setAccepted(new Taxon(sn));
+      }
     }
   }
 
